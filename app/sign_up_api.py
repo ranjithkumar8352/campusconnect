1e9a19b282c258d7b1497b2bb9d464e50bff9a55
Sw32 <sarthakshetty1@gmail.com>
2016-07-05 17:33:26 +0530

    Fixed

--- app/sign_up_api.py
+++ app/sign_up_api.py
@@ -8,13 +8,13 @@ def sign_up(user, form_data):
     user_profile = dict()
     user_profile["profileName"] = user.firstname + " " + user.lastname
     user_profile["collegeId"] = form_data["college"]
-    if "batch" in user_profile:
+    if "batch" in form_data:
         user_profile["batchName"] = form_data["batch"]
-    if "branch" in user_profile:
+    if "branch" in form_data:
         user_profile["branchName"] = form_data["branch"]
-    if "section" in user_profile:
+    if "section" in form_data:
         user_profile["sectionName"] = form_data["section"]
-    if "gcm" in user_profile:
+    if "gcm" in form_data:
         user_profile["gcmId"] = form_data["gcm"]
     user_profile["photoUrl"] = user.image_url
     user_profile["email"] = user.email
